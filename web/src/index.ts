a734cc376d3add8cfedf3885d323cd85025a7c63
Sandra Coburn <sandracoburn@ezs-macbook-pro.lan>
2021-06-10 13:02:35 -0700

    Created a User class, added optional Interface Properties

--- web/src/index.ts
+++ web/src/index.ts
@@ -1,1 +1,7 @@
-console.log('Hi there');
+import { User } from './models/Users';
+
+const user = new User({ name: 'myname', age: 20 });
+user.set({ name: 'newname' });
+
+console.log(user.get('name'));
+console.log(user.get('age'));
