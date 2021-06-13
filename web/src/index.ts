8a4b8ac0d31075ad55ef867c40c0f1175e5f9c50
Sandra Coburn <sandracoburn@ezs-macbook-pro.lan>
2021-06-12 18:15:40 -0700

    Examplee of using fetch methos

--- web/src/index.ts
+++ web/src/index.ts
@@ -1,21 +1,4 @@
 import { User } from './models/Users';
-import axios from 'axios';
 
-// const user = new User({ name: 'myname', age: 20 });
-// user.set({ name: 'newname' });
-// user.on('change', () => {
-//   console.log('change 1');
-// });
-// user.on('change', () => {
-//   console.log('change 2');
-// });
-// user.on('save', () => {
-//   console.log('Save was triggered');
-// });
-// user.trigger('change');
-
-// console.log(user);
-// // console.log(user.get('age'));
-
-axios.post('http://localhost:3000/users', { name: 'myName', age: 30 });
-axios.get('http://localhost:3000/users/1');
+const user = new User({ id: 1 });
+user.fetch();
